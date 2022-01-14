6dcdb8eee0d19d2e9ab78377876eb5b57c61b073
Zolival <zolinak@gmail.com>
2022-01-14 22:43:13 +0100

    tested ok

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: any[] = [
+    {id: 0, name: `Bionic`, photo: `img1`, sex: `female`, age: 22, health: 100,},
+    {id: 1, name: `Bionic`, photo: `img2`, sex: `female`, age: 32, health: 100,},
+    {id: 2, name: `Bionic`, photo: `img3`, sex: `male`, age: 1772, health: 100,}
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +21,9 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: any[] = [
+    {id: 0, name: `Bionic`, wings:  52, wheels: 22, clan: `cl100`,},
+    {id: 1, name: `Dynamo`, wings:  22, wheels: 1432, clan: `cl100`,},
+    {id: 2, name: `Tornado`, wings:  182, wheels: 1772, clan: `cl100`,}
+];
+   
